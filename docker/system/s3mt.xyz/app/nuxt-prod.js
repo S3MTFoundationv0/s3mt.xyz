c73103e4d63e207777cc0aa54e6bceffb0e6e3c8
web-mech <webmech@gmail.com>
2025-04-24 09:19:23 -0400

    feat(nuxt-prod): Add Nuxt public environment variables for Solana RPC URL
    Added the SOLANA_RPC_URL environment variable to the Nuxt production setup script to configure the Solana RPC URL for the public environment.

--- docker/system/s3mt.xyz/app/nuxt-prod.js
+++ docker/system/s3mt.xyz/app/nuxt-prod.js
@@ -14,41 +14,15 @@ const safeJsonParse = (str, fallback = {}) => {
 const setupNuxtEnv = () => {
   // Handle root level config
   process.env.NUXT_COMMIT_SHA_FRONT = originalEnv.COMMIT_SHA_FRONT
-  process.env.NUXT_REDIS_READER = safeJsonParse(originalEnv.REDIS_READER)
-  process.env.NUXT_REDIS_PRIMARY = safeJsonParse(originalEnv.REDIS_PRIMARY)
-  process.env.NUXT_ENABLE_REDIS_LOGGING =
-    originalEnv.ENABLE_REDIS_LOGGING === '1'
-  process.env.NUXT_ACS_CONFIG = {
-    endpoint: originalEnv.ACS_SEARCH_ENDPOINT || '',
-    region: originalEnv.ACS_REGION || '',
-    maxAttempts: 3,
-    credentials: {
-      accessKeyId: originalEnv.ACS_KEY || '',
-      secretAccessKey: originalEnv.ACS_SECRET || '',
-    },
-  }
-  process.env.NUXT_LAUNCHPAD_API_PRIVATE_URL = originalEnv.LAUNCHPAD_API_PRIVATE_URL
-  process.env.NUXT_JWT_SECRET = originalEnv.JWT_SECRET
-  process.env.NUXT_JWT_PRIVATE_KEY = ( originalEnv.JWT_PRIVATE_KEY || '' ).replace(/\\n/g, '\n')
-  process.env.NUXT_JWT_PUBLIC_KEY = (originalEnv.JWT_PUBLIC_KEY || '').replace(
-    /\\n/g,
-    '\n'
-  )
-  
-
   // Handle public config
   process.env.NUXT_PUBLIC_COMMIT_SHA_FRONT = originalEnv.COMMIT_SHA_FRONT
   process.env.NUXT_PUBLIC_CLIENT_REQUEST_TIMEOUT =originalEnv.CLIENT_REQUEST_TIMEOUT
-  process.env.NUXT_PUBLIC_PUBLIC_VAPID_KEY = originalEnv.PUBLIC_VAPID_KEY
-  process.env.NUXT_PUBLIC_API_URL = originalEnv.API_URL
-  process.env.NUXT_PUBLIC_API_PORT = originalEnv.API_PORT
-  process.env.NUXT_PUBLIC_APP_HOST = originalEnv.APP_HOST
   process.env.NUXT_PUBLIC_SOLANA_NETWORK = originalEnv.SOLANA_NETWORK
+  process.env.NUXT_PUBLIC_SOLANA_RPC_URL = originalEnv.SOLANA_RPC_URL
   process.env.NUXT_PUBLIC_PRESALE_PROGRAM_ID = originalEnv.PRESALE_PROGRAM_ID
   process.env.NUXT_PUBLIC_PRESALE_END_DATE = originalEnv.PRESALE_END_DATE
   process.env.NUXT_PUBLIC_USDC_MINT = originalEnv.USDC_MINT
   process.env.NUXT_PUBLIC_TREASURY = originalEnv.TREASURY
-  process.env.NUXT_PUBLIC_SOLANA_RPC_URL = originalEnv.SOLANA_RPC_URL
 }
 
 // Run the setup
