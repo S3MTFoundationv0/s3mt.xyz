f31d56f2979db1d2dc327a4355e3ef8b65bca88f
web-mech <webmech@gmail.com>
2025-04-28 09:38:41 -0400

    feat(useSWV): remove obsolete wallet adapters and update walletOptions configuration

--- docker/system/s3mt.xyz/app/composables/useSWV.ts
+++ docker/system/s3mt.xyz/app/composables/useSWV.ts
@@ -1,15 +1,6 @@
 import { initWallet } from "solana-wallets-vue";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
 
 const walletOptions = {
-  wallets: [
-    new PhantomWalletAdapter(),
-    new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-  ],
   autoConnect: true,
 };
 
