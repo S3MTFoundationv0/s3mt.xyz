af9d9bdf0fa7ca92e65036bc23dcaccc7018c971
web-mech <webmech@gmail.com>
2025-04-28 22:54:54 -0400

    feat(make/build.mk): Add logging to display service name, tag, type, and HAProxy file in build script

--- make/build.mk
+++ make/build.mk
@@ -2,11 +2,12 @@
 
 # Build an image for a given service s=[service]
 build:
-	@$(MAKE) _build service=$(call get_service) tag=$(call get_tag) type=$(call get_type) ha_proxy_file=$(call get_ha_proxy_file)
+	@$(MAKE) _build service=$(call get_service) tag=$(call get_tag) type=$(call get_type) ha_proxy_file=haproxy_prod.cfg
 
 dbuild:
 	@$(MAKE) _build service=$(call get_service) tag=$(call get_tag) type=$(call get_type) ha_proxy_file=haproxy_dev.cfg
 
 # Build an image for a given service service=[service]
 _build:
+	echo "Building image for $(service) with tag $(tag) and type $(type) and ha_proxy_file $(ha_proxy_file)"
 	@./scripts/build.sh $(service) $(tag) $(type) $(ha_proxy_file)
