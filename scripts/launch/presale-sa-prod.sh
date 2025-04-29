bfdd02a4d86d27d727b837c2a9f4271245331056
web-mech <webmech@gmail.com>
2025-04-28 23:17:27 -0400

    feat(scripts): add source line to presale-sa-prod.sh
    Add source line to load environment variables from docker/compose/.env file in presale-sa-prod.sh script.

--- scripts/launch/presale-sa-prod.sh
+++ scripts/launch/presale-sa-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+source docker/compose/.env
 
 docker run -d --rm --name s3mt-presale-standalone\
     -p 80:80\
     -p 443:443\
     --env-file docker/.config/.env.shared\
 	--env-file docker/.config/.env.s3mt.xyz-prod\
-	ghcr.io/s3mtfoundation/s3mt.xyz:latest
+	ghcr.io/${ORGANIZATION}/s3mt.xyz:latest
