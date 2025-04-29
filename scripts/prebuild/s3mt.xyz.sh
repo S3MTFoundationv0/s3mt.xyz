731b6acf42ff24dd6896abd32320bdb0163260e1
web-mech <webmech@gmail.com>
2025-04-28 23:46:24 -0400

    fix(prebuild): Update SSL certificate paths in s3mt.xyz.sh
    - Updated paths for storing SSL certificates in the s3mt.xyz.sh script to ensure correct location for certificates.
    - Modified the directory structure to be more organized and accurate.
    - Prior paths were incorrect and could lead to confusion or errors during SSL certificate setup.

--- scripts/prebuild/s3mt.xyz.sh
+++ scripts/prebuild/s3mt.xyz.sh
@@ -2,16 +2,16 @@
 
 if [ -n "$FULLCHAIN_TLS" ]; then
     echo "FULLCHAIN is set. Setting up SSL certificate."
-    mkdir -p docker/system/s3mt.xyz/etc/ssl/
-    echo "$FULLCHAIN_TLS" | base64 --decode > docker/system/s3mt.xyz/etc/ssl/beta.s3mt.xyz.fullchain.pem
+    mkdir -p docker/system/s3mt.xyz/etc/ssl/s3mt.xyz
+    echo "$FULLCHAIN_TLS" | base64 --decode > docker/system/s3mt.xyz/etc/ssl/s3mt.xyz/beta.s3mt.xyz.fullchain.pem
 fi
 
 
 
 if [ -n "$FULLCHAIN_TLS_PROD" ]; then
     echo "FULLCHAIN_PROD is set. Setting up SSL certificate."
-    mkdir -p docker/system/s3mt.xyz/etc/ssl/
-    echo "$FULLCHAIN_TLS_PROD" | base64 --decode > docker/system/s3mt.xyz/etc/ssl/s3mt.xyz.fullchain.pem
+    mkdir -p docker/system/s3mt.xyz/etc/ssl/s3mt.xyz
+    echo "$FULLCHAIN_TLS_PROD" | base64 --decode > docker/system/s3mt.xyz/etc/ssl/s3mt.xyz/s3mt.xyz.fullchain.pem
 fi
 
 
