fc97a16b5ff3a6896f9f13d259c0addc8dc3ce85
web-mech <webmech@gmail.com>
2025-05-12 17:23:06 -0400

    feat(app): add Google Tag Manager script
    - Added Google Tag Manager script in the app.vue file to enable tracking for GTM-W5X3RJ8D.

--- docker/system/s3mt.xyz/app/app.vue
+++ docker/system/s3mt.xyz/app/app.vue
@@ -1,4 +1,7 @@
 <template>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5X3RJ8D" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div class="min-h-screen bg-gray-900 text-white flex flex-col">
     <NavBar />
     <main class="flex-grow">
